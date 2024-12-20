3803e0183da6bca5ae6552a9cc97ca1063c848c1
Mateusz Goślinowski <mateusz.g@mlabs.city>
2024-12-20 19:13:11 +0100

    Add dev run shell source

---
+++ run_dev_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+cabal build -v0 --project-file=cabal-ghcjs.project all
+cp "./dist-newstyle/build/javascript-ghcjs/ghc-9.8.2/haskell-halogen-core-0.1.0.0/x/halogen-core-prototype/build/halogen-core-prototype/halogen-core-prototype.jsexe/all.js" "dev/index.js"
+http-server dev/
